2fc2b74afe758fbb5b2f570cd01614dfe6599d2f
cewald <hello@christianewald.de>
2022-10-25 09:48:07 +0000

    Improve linting

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -11,5 +11,13 @@ module.exports = {
   ],
   parserOptions: {
     ecmaVersion: 'latest'
-  }
+  },
+  overrides: [
+    {
+      files: ['tailwind.config.js', 'postcss.config.js'],
+      env: {
+        node: true
+      }
+    }
+  ]
 }
