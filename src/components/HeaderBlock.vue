5ae84ccfe707ef8142f1815602fba5a4b0108fb0
cewald <hello@christianewald.de>
2022-10-26 09:20:25 +0000

    Create pixel-perfect header

--- src/components/HeaderBlock.vue
+++ src/components/HeaderBlock.vue
@@ -1,14 +1,44 @@
 <script setup lang="ts">
-import Logo from '@/assets/logo.svg?component'
+import Logo from '@/assets/logo.svg?url'
 </script>
 
 <template>
-  <header>
-    <Logo />
-    <h1>Christian Ewald</h1>
-    <p>
-      senior full-stack javascript developer<br />
-      and software-engineer
-    </p>
+  <header class="header mt-16 flex font-mono text-gray-default">
+    <div class="header__logo flex items-start justify-end">
+      <img :src="Logo" class="fill-gray-default" />
+    </div>
+    <div class="header__title antialiased">
+      <h1 class="font-bold">Christian Ewald</h1>
+      <p class="">
+        senior full-stack javascript developer<br />
+        and software-engineer
+      </p>
+    </div>
   </header>
 </template>
+
+<style lang="scss" scoped>
+header {
+  .header__logo {
+    width: 16rem;
+
+    img {
+      width: 9rem;
+    }
+  }
+
+  .header__title {
+    margin: 2.9rem 0 0 1.5rem;
+    line-height: 1.55rem;
+
+    h1 {
+      font-size: 2.225rem;
+      margin-bottom: 0.7rem;
+    }
+
+    p {
+      font-size: 1.11rem;
+    }
+  }
+}
+</style>
--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,17 @@
+const defaultTheme = require('tailwindcss/defaultTheme')
+
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
   theme: {
     extend: {
+      fontFamily: {
+        sans: ['Roboto', ...defaultTheme.fontFamily.sans],
+        mono: ['Roboto Mono', ...defaultTheme.fontFamily.mono]
+      },
+      colors: {
+        'gray-default': '#262626'
+      },
       boxShadow: {
         cnt: '0 0 15px -5px rgb(0 0 0 / 0.1), 0 2px 10px -6px rgb(0 0 0 / 0.1)'
       }
