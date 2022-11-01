dbe2ce5e1d897b12134da011989a8df970060b40
cewald <hello@christianewald.de>
2022-11-01 11:38:13 +0000

    First draft for gantt

--- src/components/SkillGantt.vue
+++ src/components/SkillGantt.vue
@@ -1,8 +1,37 @@
 <template>
-  <div v-for="section in skillset" :key="section.section" class="hidden">
-    <h3>{{ section.section }}</h3>
-    <div v-for="skill in section.skills" :key="skill.title">
-      {{ skill.timeslots }} – {{ skill.title }}
+  <div class="flex justify-between">
+    <div class="pl-4">{{ startDate.getFullYear() }}</div>
+    <div>{{ endDate.getFullYear() }}</div>
+  </div>
+  <div
+    v-for="{ section, skills } in skillsetStruct"
+    :key="section"
+    class="flex flex-wrap"
+  >
+    <div class="flex w-4 flex-fix">
+      <div class="-rotate-90">{{ section }}</div>
+    </div>
+    <div class="grow">
+      <div
+        v-for="{ title, percentTimeslots } in skills"
+        :key="title"
+        class="mb-2 flex"
+      >
+        <div class="flex w-full">
+          <div
+            :style="{ width: percentTimeslots[0].start + '%' }"
+            class="font-mono"
+          >
+            {{ title }}
+          </div>
+          <div
+            v-for="({ start, stop }, i) in percentTimeslots"
+            :key="`bar-${title}-${i}`"
+            class="h-full rounded bg-gray-lighter opacity-30"
+            :style="{ width: 100 - start - ((stop || 0) - start) + '%' }"
+          />
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -14,7 +43,7 @@ type SkillSetSkill = {
   title: string
   timeslots: string[]
   timestampedTimeslots: { start: Date; stop?: Date }[]
-  percentTimeslots: { start: Number; stop?: Number }[]
+  percentTimeslots: { start: number; stop?: number }[]
 }
 
 type SkillSetSection = {
@@ -25,21 +54,26 @@ type SkillSetSection = {
 export default {
   setup() {
     const skillset = ref(Skillset)
+    const endDate = ref(new Date())
+
     return {
-      skillset
+      skillset,
+      endDate
     }
   },
   computed: {
     skillsetStruct() {
-      return ([...Skillset] as SkillSetSection[]).map((section) => {
+      return ([...this.skillset] as SkillSetSection[]).map((section) => {
         section.skills.map((skill) => {
           skill.timestampedTimeslots = skill.timeslots.map((s) => {
             const [start, stop] = s.split('-').map((v) => new Date(v))
             return { start, stop }
           })
 
-          const startTS = this.startDate.getTime()
-          const endTS = this.endDate.getTime()
+          skill.percentTimeslots = skill.timestampedTimeslots.map((i) => ({
+            start: this.timeStampToPercent(i.start),
+            stop: i.stop ? this.timeStampToPercent(i.stop) : undefined
+          }))
 
           return skill
         })
@@ -60,9 +94,15 @@ export default {
 
       const firstYear = slots.reduce((a, b) => (a > b ? b : a))
       return new Date(firstYear.toString())
-    },
-    endDate() {
-      return new Date()
+    }
+  },
+  methods: {
+    timeStampToPercent(timestamp: Date): number {
+      const start = this.startDate.getTime()
+      const end = this.endDate.getTime()
+      const scale = end - start
+      const point = timestamp.getTime() - start
+      return Math.round((point * 100) / scale)
     }
   }
 }
