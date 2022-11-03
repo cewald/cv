da8b1d8f82cf8dddae7fbda23b2517e6171196f2
cewald <hello@christianewald.de>
2022-11-03 12:08:16 +0000

    Update `vite-plugin-webfont-dl` for bugfix * See: https://github.com/feat-agency/vite-plugin-webfont-dl/issues/18#issuecomment-1301922866

--- vite.config.ts
+++ vite.config.ts
@@ -14,9 +14,13 @@ export default defineConfig({
     vue(),
     vueJsx(),
     svgLoader(),
-    webfontDownload([
-      'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&family=Roboto+Mono:wght@400;700&display=swap'
-    ]),
+    webfontDownload(
+      [
+        'https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap',
+        'https://fonts.googleapis.com/css2?family=Roboto+Mono:wght@400;700&display=swap'
+      ],
+      { injectAsStyleTag: false, minifyCss: true }
+    ),
     Icons({})
   ],
   resolve: {
